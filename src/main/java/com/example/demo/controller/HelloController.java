939bf1a708e0a8cb17accc064b6991b1c40ed69e
linskin <1360293790@qq.com>
2023-06-03 20:05:14 +0800

    新增了一些注释

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,15 +3,35 @@ package com.example.demo.controller;
 import com.example.demo.bean.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    /**
+     *该controller一般被用于测试
+     */
     @Autowired
     Person person;
     @RequestMapping("/person")
     public Person person(){
         return person;
     }
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "GET-张三";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String setUser(){
+        return "SET-张三";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "PUT-张三";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-张三";
+    }
+
 }
